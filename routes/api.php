913156e0560c246002470b9e7f88e3ae2862687b
Elton Rexha <elton.rexha123@outlook.com>
2025-03-22 13:59:32 +0100

    inital commit

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+
+Route::controller(AuthController::class)->prefix("auth")->middleware('api')->group(function () {
+    Route::post('login', 'login')->name('auth.login');
+    Route::post('register', 'register')->name('auth.register');
+    Route::post('refresh', 'refresh')->name('auth.refresh');
+
+
+    Route::middleware('jwt.auth.token')->group(function () {
+        Route::post("logout", "logout")->name("auth.logout");
+        Route::get('user-profile', 'userProfile')->name('auth.user.profile');
+    });
+
+   
+    // kriju shume api
+});
